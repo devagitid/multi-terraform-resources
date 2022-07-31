9e0a05d2c1640719adda164169dcee7cacc112b3
devagitid <104121621+devagitid@users.noreply.github.com>
2022-07-31 20:32:44 +0530

    created new azure resource

--- devresource/main.tf
+++ devresource/main.tf
@@ -6,3 +6,28 @@ module "sqldbresource" {
 source = "../sqldbmodule/"
 resource_env = "dev"
 }
+
+
+resource "azurerm_key_vault" "kv" {
+  name                       = "keyvault01"
+  location                   = azurerm_resource_group.sqldb.location
+  resource_group_name        = azurerm_resource_group.sqldb.name
+  sku_name                   = "premium"
+  soft_delete_retention_days = 7
+
+  access_policy {
+   
+      azure_ad_user_principal_names = ["devdattajadhav79gmail.onmicrosoft.com"]
+      key_permissions               = ["get", "list"]
+      secret_permissions            = ["get", "list"]
+      certificate_permissions       = ["get", "import", "list"]
+      storage_permissions           = ["backup", "get", "list", "recover"]
+    
+  }
+}
+
+resource "azurerm_key_vault_secret" "kv" {
+  name         = azurerm_sql_server.sqldb.administrator_login
+  value        = azurerm_sql_server.sqldb.administrator_login_password
+  key_vault_id = azurerm_key_vault.kv.id
+}
