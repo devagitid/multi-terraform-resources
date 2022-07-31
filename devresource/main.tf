83d29615bea292fb68a03eb112cd2601fffb8a28
devagitid <104121621+devagitid@users.noreply.github.com>
2022-07-31 21:46:33 +0530

    updated module reference

--- devresource/main.tf
+++ devresource/main.tf
@@ -7,32 +7,37 @@ source = "../sqldbmodule/"
 resource_env = "dev"
 }
 
-data "azurerm_resource_group" "sqldb" {
-}
-  
-data "azurerm_sql_server" "sqldb" {
-}
+data "azurerm_client_config" "current" {}
   
 resource "azurerm_key_vault" "kv" {
   name                       = "keyvault01"
-  location                   = module.sqldbresource.azurerm_sql_server.sqldb.location
-  resource_group_name        = module.sqldbresource.azurerm_sql_server.sqldb.name
+  location                   = module.sqldbresource.azurerm_resource_group.sqldb.location
+  resource_group_name        = module.sqldbresource.azurerm_resource_group.sqldb.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "premium"
   soft_delete_retention_days = 7
 
   access_policy {
    
-      azure_ad_user_principal_names = ["devdattajadhav79gmail.onmicrosoft.com"]
+      azure_ad_user_principal_names = ["Deva@devdattajadhav79gmail.onmicrosoft.com"]
       key_permissions               = ["get", "list"]
       secret_permissions            = ["get", "list"]
       certificate_permissions       = ["get", "import", "list"]
       storage_permissions           = ["backup", "get", "list", "recover"]
     
   }
+  
+  depends_on = [
+    module.sqldbresource.azurerm_resource_group.sqldb
+  ]
 }
 
 resource "azurerm_key_vault_secret" "kv" {
-  name         = data.azurerm_sql_server.sqldb.administrator_login
-  value        = data.azurerm_sql_server.sqldb.administrator_login_password
+  name         = module.sqldbresource.azurerm_sql_server.sqldb.administrator_login
+  value        = module.sqldbresource..azurerm_sql_server.sqldb.administrator_login_password
   key_vault_id = azurerm_key_vault.kv.id
+  
+  depends_on = [
+    module.sqldbresource..azurerm_sql_server.sqldb
+  ]
 }
